8437ef8928bd4d1bd4ee1271b0edc80f1795a05c
dkizimova <diljara06@gmail.com>
2025-01-09 12:31:16 +0300

    Update developer.py

--- developer.py
+++ developer.py
@@ -13,19 +13,8 @@ class House:
             else:
                 print(i)
 
-    def __len__(self):
-        return self.number_of_floors
-
-    def __str__(self):
-        return (f'Название: {self.name}, количество этажей: {self.number_of_floors}')
-
 h1=House('ЖК Горский', 18)
 h2=House('Домик в деревне', 2)
-#h1.to_go(5)
-#h2.to_go(10)
+h1.to_go(5)
+h2.to_go(10)
 
-print(h1)
-print(h2)
-
-print(len(h1))
-print(len(h2))
