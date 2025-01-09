28e6ea512442316f003f11e26e71f1f0ffa18d5f
dkizimova <diljara06@gmail.com>
2025-01-09 11:08:46 +0300

    Add files via upload
    Атрибуты и методы объекта

---
+++ developer.py
@@ -0,0 +1,21 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def to_go(self, new_floor):
+        new_floor = int(new_floor)
+        for i in range (1, new_floor+1):
+
+            if new_floor < 1 or new_floor > self.number_of_floors:
+                print("Такого этажа не существует")
+                break
+            else:
+                print(i)
+
+
+
+h1=House('ЖК Горский', 18)
+h2=House('Домик в деревне', 2)
+h1.to_go(5)
+h2.to_go(10)
